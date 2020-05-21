38888a63d39de4f1d1498d4d34501bfacac7ca27
Francisco Giovanildo Teixeira de Souza <giovanildos@gmail.com>
2020-05-21 03:28:06 -0300

    iniciando página frequenciaMensal

--- src/main/java/com/giovanildo/dao/JPAUtil.java
+++ src/main/java/com/giovanildo/dao/JPAUtil.java
@@ -7,7 +7,7 @@ import javax.persistence.Persistence;
 public class JPAUtil {
 
 	private static EntityManagerFactory emf = Persistence
-			.createEntityManagerFactory("sigtemp");
+			.createEntityManagerFactory("sigtestes");
 
 	public EntityManager getEntityManager() {
 		return emf.createEntityManager();
